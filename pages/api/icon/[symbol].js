3720c1368aee50029b1b934580ef27994022a0cf
Faris Aziz <farisaziz12@gmail.com>
2022-07-29 16:27:04 +0200

    use js and fix api static files resp

--- pages/api/icon/[symbol].js
+++ pages/api/icon/[symbol].js
@@ -1,19 +1,17 @@
-import { NowRequest, NowResponse } from "@vercel/node";
 import path from "path";
 import fs from "fs";
 
-export default (req: NowRequest, res: NowResponse) => {
+export default (req, res) => {
   const { symbol } = req.query;
 
-  let filePath = path.join(
-    __dirname,
-    `../../public/icons/128/color/${symbol}.png`
-  );
+  const publicDirectory = path.join(process.cwd(), "public");
+
+  let filePath = path.join(publicDirectory, `/icons/128/color/${symbol}.png`);
 
   if (!fs.existsSync(filePath)) {
     // If file doesn't exist (no icon for the given [symbol]) => Replace it with the alt image (_no_image_.png)
     // This might be a better way to handle missing icons, we can avoid 404, plus, user might based on the [_no_image_] to report the missing icon
-    filePath = path.join(__dirname, '../../public/icons/128/color/_no_image_.png');
+    filePath = path.join(publicDirectory, "/icons/128/color/_no_image_.png");
   }
 
   const readStream = fs.createReadStream(filePath);
